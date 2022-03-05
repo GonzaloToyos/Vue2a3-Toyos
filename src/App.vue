d6bab434f230a185c01b5f62f322ab8a3a890d9a
Gonzalo Toyos <gonzalotoyos2001@gmail.com>
2022-03-05 20:02:42 -0300

    Primer commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TablaEjercicio
+      :msg="miArray"
+      :text="miColor"
+      :bgp="bgPrimary"
+      :bgs="bgSecondary"
+    />
+    <TablaEjercicio
+      :msg="miArray2"
+      :text="miColor2"
+      :bgp="bgPrimary2"
+      :bgs="bgSecondary2"
+    />
+    <TablaEjercicio
+      :msg="miArray3"
+      :text="miColor3"
+      :bgp="bgPrimary3"
+      :bgs="bgSecondary3"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TablaEjercicio from "./components/MiComponente.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TablaEjercicio,
+  },
+  data() {
+    return {
+      miArray: [
+        { nombre: "Pedro", apellido: "Santos", edad: 35 },
+        { nombre: "Marcelo", apellido: "Paez", edad: 43 },
+        { nombre: "Juan", apellido: "Rutinger", edad: 23 },
+        { nombre: "Alfredo", apellido: "López", edad: 15 },
+        { nombre: "Mateo", apellido: "Sarmiento", edad: 30 },
+      ],
+      miColor: "color: white",
+      bgPrimary: "background-color: rgb(33, 37, 41)",
+      bgSecondary: "background-color: rgb(44, 48, 52)",
+      miArray2: [
+        { nombre: "Maria", apellido: "Frank", edad: 33 },
+        { nombre: "Rigoberta", apellido: "Menchú", edad: 23 },
+        { nombre: "Marta", apellido: "Palo", edad: 78 },
+        { nombre: "Victoria", apellido: "Louz", edad: 34 },
+        { nombre: "Anabella", apellido: "Simmons", edad: 40 },
+      ],
+      miColor2: "color: black",
+      bgPrimary2: "background-color: rgb(209, 231, 221)",
+      bgSecondary2: "background-color: rgb(199, 219, 210)",
+      miArray3: [
+        { nombre: "Rene", apellido: "Higuita", edad: 35 },
+        { nombre: "Lionel", apellido: "Messi", edad: 36 },
+        { nombre: "Julio", apellido: "Tolaymig", edad: 53 },
+        { nombre: "Marco", apellido: "Polo", edad: 18 },
+        { nombre: "Telesforo", apellido: "Ibarra", edad: 90 },
+      ],
+      miColor3: "color: black",
+      bgPrimary3: "background-color: rgb(0, 255, 255)",
+      bgSecondary3: "background-color: rgb(128, 255, 255)",
+    };
+  },
+};
 </script>
 
 <style>
---
+++ src/components/MiComponente.vue
@@ -0,0 +1,36 @@
+<template>
+  <table :style="estilo">
+    <tr v-for="(row, prop) of msg" :key="row" :style="color(prop)">
+      <td v-for="valor of row" :key="valor">{{ valor }}</td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "TablaEjercicio",
+  props: {
+    msg: Array,
+    text: String,
+    bgp: String,
+    bgs: String,
+  },
+  data() {
+    return {
+      estilo: this.text + "; " + "border-collapse:collapse; font-size: 24px",
+    };
+  },
+  methods: {
+    color(i) {
+      let result = i%2 ? this.bgp : this.bgs;
+      return result;
+    },
+  },
+};
+</script>
+
+<style scoped>
+table {
+  margin-bottom: 15px;
+}
+</style>
